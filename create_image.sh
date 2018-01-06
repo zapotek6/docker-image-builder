772babef851381fe1574d19958c31918fa852aea
zap <zapotek6@gmail.com>
2018-01-06 20:05:28 +0100

    add missing copy file support

--- create_image.sh
+++ create_image.sh
@@ -2,6 +2,20 @@
 
 . ./defaults
 
+copyDirectories() {
+  _SRC=${1}
+  _DST=${2}
+  cp -r ${1} ${2}
+}
+
+copyFiles() {
+  _SRC=${1}
+  _DST=${2}
+  cp  ${1} ${2}
+}
+
+##### MAIN #####
+
 VERSION="${1}"
 
 if [ -z "${VERSION}" ]; then
@@ -14,6 +28,23 @@ fi
 if [ ! -f "./Dockerfile" ]; then
   echo "ERROR Dockerfile is missing! Fatal error"
   exit 2
+fi
+
+
+if [ ! -z "${DIRS_TO_COPY}" ]; then
+  echo "Copying directories: ${DIRS_TO_COPY}"
+  for srcDir in ${DIRS_TO_COPY}
+  do
+    copyDirectories ${srcDir} .
+  done
+fi
+
+if [ ! -z "${FILES_TO_COPY}" ]; then
+  echo "Copying files: ${FILES_TO_COPY}"
+  for srcFile in ${FILES_TO_COPY}
+  do
+    copyFiles ${srcFile} .
+  done
 fi
 
 sudo docker build . -t "${IMAGE}:${VERSION}"
